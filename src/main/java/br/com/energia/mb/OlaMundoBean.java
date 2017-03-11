c1645cb70d5bd7542b7aead7d09dd0a024263191
rafael <rafaell.elias@hotmail.com>
2017-03-11 17:37:32 -0300

    Novas att

--- src/main/java/br/com/energia/mb/OlaMundoBean.java
+++ src/main/java/br/com/energia/mb/OlaMundoBean.java
@@ -7,9 +7,32 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class OlaMundoBean {
-	
-	public String getHorario() {
-	    SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
-	    return "Atualizado em " + sdf.format(new Date(0));
+
+	private String nome;
+	private String mensagem = "Olá quem é você";
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String nomeFoiDigitado() {
+	    return "O nome do maluco é "+getNome();
 	  }
+
+	public String getHorario() {
+		SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+		return "Atualizado em " + sdf.format(new Date(0));
+	}
 }
